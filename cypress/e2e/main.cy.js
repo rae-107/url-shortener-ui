baea9e8ef35539d0402e755429a2dd390e3e8314
Rae Gebhart <raegebhart@gmail.com>
2023-03-07 10:40:33 -0700

    Move newUrl cypress test to it's own file

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -14,6 +14,8 @@ describe('Main Page', () => {
 
   it('Should display form to shorten a url', () => {
     cy.get('input').should('have.length', 2)
+    cy.get('input[name=title]').should('be.visible')
+    cy.get('input[name=urlToShorten]').should('be.visible')
     cy.get('button').contains('Shorten Please!')
   })
 
@@ -22,15 +24,16 @@ describe('Main Page', () => {
     cy.get('.url').contains('http://localhost:3001/useshorturl/1')
     cy.get('.url').contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
   })
+
+  it('Should be able to type into form', () => {
+    cy.get('input[name=title]').type('Stack overflow')
+    cy.get('input[name=urlToShorten]').type('https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe')
+    cy.get('input[name=title]').should('have.value', 'Stack overflow')
+    cy.get('input[name=urlToShorten]').should('have.value', 'https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe')
+  })
 })
 
-// Iteration 3
-// Write Cypress tests for the following user flows (don't forget to stub your network requests):
 
-// When a user visits the page, they can view the page title and the existing shortened URLs
-// When a user visits the page, they can view the Form with the proper inputs
-// When a user fills out the form, the information is reflected in the input fields
-// Iteration 4
-// Write Cypress tests for the following user flows (don't forget to stub your network requests):
 
-// When a user fills out and submits the form, the new shortened URL is rendered
+// What should happen if the server sends back a failed request?
+// What should happen if the user tries to submit an incomplete form?

90ef94c407b5c09837861448c79771a2238ede52
Rae Gebhart <raegebhart@gmail.com>
2023-03-07 11:52:16 -0700

    Update post test

--- cypress/e2e/newUrl.cy.js
+++ cypress/e2e/newUrl.cy.js
@@ -4,24 +4,22 @@ describe("New Url", () => {
       { method: "POST", url: "http://localhost:3001/api/v1/urls" },
       {
         statusCode: 200,
-        headers: { 
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          title: "Stack OverFlow",
-          long_url:
-            "https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe",
-        })
+        body: {
+          "id": 2,
+          "long_url": "https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe",
+          "short_url": "http://localhost:3001/useshorturl/2",
+          "title": "Stack Overflow"
+        }
       }
     );
-    cy.intercept('GET', "http://localhost:3001/api/v1/urls", {fixture: 'newUrl.json'})
     cy.visit("http://localhost:3000/");
-    cy.get("input[name=title]").type("Stack overflow");
+    cy.get("input[name=title]").type("Stack Overflow");
     cy.get("input[name=urlToShorten]").type(
       "https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe"
       );
     cy.get("button").click();
-    cy.get(".url").eq(1).contains("Stack Overflow");
+    cy.get(".url").last().contains("Stack Overflow");
+    cy.get(".url").last().contains("http://localhost:3001/useshorturl/2");
     cy.get(".url")
       .last()
       .contains(
